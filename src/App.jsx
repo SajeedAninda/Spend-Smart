98502f1b786396b25b929bca285c2a16010b95b9
SajeedAninda <sajaninda@gmail.com>
2025-01-31 17:13:35 +0600

    Created components for hompage and hero

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import Navbar from "./Components/Navbar/Navbar"
+import Homepage from "./Components/Navbar/Pages/Homepage/Homepage"
 
 function App() {
 
   return (
     <>
       <Navbar></Navbar>
+      <Homepage></Homepage>
     </>
   )
 }
