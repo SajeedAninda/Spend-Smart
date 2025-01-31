840f4a632e80c0c0f8fd686076e4696b7c32f88c
SajeedAninda <sajaninda@gmail.com>
2025-01-31 18:09:20 +0600

    Created a button Component

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -3,11 +3,13 @@ import logo from '../../assets/spend-smart-logo.png'
 import { IoLogIn } from 'react-icons/io5'
 import { TypeAnimation } from 'react-type-animation'
 import { Link } from 'react-router-dom'
+import MainButton from '../Shared/MainButton'
 
 const Navbar = () => {
   return (
-    <div className='w-full h-[14vh] bg-gradient-to-r from-[#30e4ba]  via-white to-[#30e4ba] flex items-center'>
+    <div className='w-full h-[14vh] bg-gradient-to-r from-[#30e4ba] via-white to-[#30e4ba] flex items-center'>
       <div className='w-[1000px] mx-auto flex justify-between items-center'>
+        {/* Brand Name + Tagline */}
         <div className='w-[33%] flex flex-col items-center text-center'>
           <h1 className='text-[#02101c] font-black text-[16px] italic'>
             SpendSmart
@@ -29,28 +31,20 @@ const Navbar = () => {
           />
         </div>
 
+        {/* Logo */}
         <div className='w-[33%] flex justify-center'>
-          <Link to={"/"}>
-          <img className='w-[120px] cursor-pointer' src={logo} alt='' />
+          <Link to={'/'}>
+            <img className='w-[120px] cursor-pointer' src={logo} alt='' />
           </Link>
         </div>
-        <div className='w-[33%] flex justify-end'>
-          <div class='flex items-center justify-center h-screen'>
-            <div class='relative group'>
-              <button class='relative inline-block p-px font-semibold leading-6 text-white bg-[#02101c] shadow-lg cursor-pointer rounded-xl shadow-zinc-900 transition-transform duration-300 ease-in-out hover:scale-105 active:scale-95'>
-                <span class='absolute inset-0 rounded-xl bg-gradient-to-r from-teal-400 via-blue-500 to-white p-[2px] opacity-0 transition-opacity duration-500 group-hover:opacity-100'></span>
 
-                <span class='relative z-10 block px-6 py-3 rounded-xl bg-[#02101c]'>
-                  <div class='relative z-10 flex items-center space-x-2'>
-                    <span class='transition-all duration-500 group-hover:translate-x-1'>
-                      Login
-                    </span>
-                    <IoLogIn className='w-6 h-6 transition-transform duration-500 group-hover:translate-x-1' />
-                  </div>
-                </span>
-              </button>
-            </div>
-          </div>
+        {/* Buttons */}
+        <div className='w-[33%] flex justify-end space-x-4'>
+          <MainButton
+            text='Login'
+            icon={IoLogIn}
+            onClick={() => console.log('Login Clicked')}
+          />
         </div>
       </div>
     </div>
---
+++ src/Components/Shared/MainButton.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+
+const MainButton = ({ text, icon: Icon, onClick, className = '' }) => {
+  return (
+    <div className='relative group'>
+      <button
+        className={`relative inline-block p-px font-semibold leading-6 text-white bg-[#02101c] shadow-lg cursor-pointer rounded-xl shadow-zinc-900 transition-transform duration-300 ease-in-out hover:scale-105 active:scale-95 ${className}`}
+        onClick={onClick}
+      >
+        <span className='absolute inset-0 rounded-xl bg-gradient-to-r from-teal-400 via-blue-500 to-white p-[2px] opacity-0 transition-opacity duration-500 group-hover:opacity-100'></span>
+
+        <span className='relative z-10 block px-6 py-3 rounded-xl bg-[#02101c]'>
+          <div className='relative z-10 flex items-center space-x-2'>
+            <span className='transition-all duration-500 group-hover:translate-x-1'>
+              {text}
+            </span>
+            {Icon && (
+              <Icon className='w-6 h-6 transition-transform duration-500 group-hover:translate-x-1' />
+            )}
+          </div>
+        </span>
+      </button>
+    </div>
+  )
+}
+
+export default MainButton
