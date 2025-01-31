f975deb8dfd0ff2ded6df656a27998552c0acadd
SajeedAninda <sajaninda@gmail.com>
2025-01-31 18:03:51 +0600

    Created texts and buttons of hero section

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -40,7 +40,7 @@ const Navbar = () => {
               <button class='relative inline-block p-px font-semibold leading-6 text-white bg-[#02101c] shadow-lg cursor-pointer rounded-xl shadow-zinc-900 transition-transform duration-300 ease-in-out hover:scale-105 active:scale-95'>
                 <span class='absolute inset-0 rounded-xl bg-gradient-to-r from-teal-400 via-blue-500 to-white p-[2px] opacity-0 transition-opacity duration-500 group-hover:opacity-100'></span>
 
-                <span class='relative z-10 block px-6 py-3 rounded-xl bg-gray-950'>
+                <span class='relative z-10 block px-6 py-3 rounded-xl bg-[#02101c]'>
                   <div class='relative z-10 flex items-center space-x-2'>
                     <span class='transition-all duration-500 group-hover:translate-x-1'>
                       Login
--- src/Components/Navbar/Pages/Homepage/Hero/Hero.jsx
+++ src/Components/Navbar/Pages/Homepage/Hero/Hero.jsx
@@ -1,11 +1,65 @@
-import React from 'react';
+import React from 'react'
+import { IoLogIn } from 'react-icons/io5'
+import { SiKnowledgebase } from 'react-icons/si'
+import { VscDebugStart } from 'react-icons/vsc'
 
 const Hero = () => {
-    return (
-        <div>
-            
-        </div>
-    );
-};
+  return (
+    <section class='pt-24 pb-32'>
+      <div class='overflow-hidden border-b border-muted'>
+        <div class='w-[1000px] mx-auto'>
+          <div class=' flex-col items-center'>
+            <div class='z-10 items-center text-center'>
+              <h1 class='mb-8 text-[#02101c] text-pretty text-4xl font-semibold lg:text-8xl'>
+                Track, Save, and Grow with SpendSmart
+              </h1>
+              <p class='mx-auto max-w-screen-md text-zinc-600 lg:text-xl'>
+                Take control of your finances with an easy-to-use tracker that
+                helps you monitor expenses, set budgets, and plan for a secure
+                future—all in one powerful platform
+              </p>
+              <div class='mt-12 flex w-full flex-col justify-center gap-6 sm:flex-row'>
+                <div className='relative group'>
+                  <button class='relative inline-block p-px font-semibold leading-6 text-white bg-[#02101c] shadow-lg cursor-pointer rounded-xl shadow-zinc-900 transition-transform duration-300 ease-in-out hover:scale-105 active:scale-95'>
+                    <span class='absolute inset-0 rounded-xl bg-gradient-to-r from-teal-400 via-blue-500 to-white p-[2px] opacity-0 transition-opacity duration-500 group-hover:opacity-100'></span>
 
-export default Hero;
+                    <span class='relative z-10 block px-6 py-3 rounded-xl bg-[#02101c]'>
+                      <div class='relative z-10 flex items-center space-x-2'>
+                        <span class='transition-all duration-500 group-hover:translate-x-1'>
+                          Get Started
+                        </span>
+                        <VscDebugStart className='w-6 h-6 transition-transform duration-500 group-hover:translate-x-1' />
+                      </div>
+                    </span>
+                  </button>
+                </div>
+
+                <div className='relative group'>
+                  <button class='relative inline-block p-px font-semibold leading-6 text-[#02101c] bg-[#30e4ba]  shadow-lg cursor-pointer rounded-xl shadow-zinc-900 transition-transform duration-300 ease-in-out hover:scale-105 active:scale-95'>
+                    <span class='absolute inset-0 rounded-xl bg-gradient-to-r from-[#02101c] via-[#023a6b] to-white p-[2px] opacity-0 transition-opacity duration-500 group-hover:opacity-100'></span>
+
+                    <span class='relative z-10 block px-6 py-3 rounded-xl bg-[#30e4ba] '>
+                      <div class='relative z-10 flex items-center space-x-2'>
+                        <span class='transition-all duration-500 group-hover:translate-x-1'>
+                          Learn More
+                        </span>
+                        <SiKnowledgebase className='w-6 h-6 transition-transform duration-500 group-hover:translate-x-1' />
+                      </div>
+                    </span>
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+          <img
+            src='/images/placeholders/minima/placeholder-wide-1.jpg'
+            alt='placeholder'
+            class='mx-auto mt-24 max-h-[700px] w-full max-w-7xl rounded-t-lg object-cover shadow-lg'
+          />
+        </div>
+      </div>
+    </section>
+  )
+}
+
+export default Hero
