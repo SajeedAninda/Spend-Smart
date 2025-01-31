5bf45459e19b806f71109001a7b731084be9b2cb
SajeedAninda <sajaninda@gmail.com>
2025-01-31 18:42:45 +0600

    Completed hero section

--- src/Components/Navbar/Pages/Homepage/Hero/Hero.jsx
+++ src/Components/Navbar/Pages/Homepage/Hero/Hero.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
-import { IoLogIn } from 'react-icons/io5'
 import { SiKnowledgebase } from 'react-icons/si'
 import { VscDebugStart } from 'react-icons/vsc'
+import MainButton from '../../../../Shared/MainButton'
+import heroImg from '../../../../../assets/hero.webp'
 
 const Hero = () => {
   return (
     <section class='pt-24 pb-32'>
-      <div class='overflow-hidden border-b border-muted'>
+      <div class='overflow-hidden'>
         <div class='w-[1000px] mx-auto'>
           <div class=' flex-col items-center'>
             <div class='z-10 items-center text-center'>
@@ -19,20 +20,11 @@ const Hero = () => {
                 future—all in one powerful platform
               </p>
               <div class='mt-12 flex w-full flex-col justify-center gap-6 sm:flex-row'>
-                <div className='relative group'>
-                  <button class='relative inline-block p-px font-semibold leading-6 text-white bg-[#02101c] shadow-lg cursor-pointer rounded-xl shadow-zinc-900 transition-transform duration-300 ease-in-out hover:scale-105 active:scale-95'>
-                    <span class='absolute inset-0 rounded-xl bg-gradient-to-r from-teal-400 via-blue-500 to-white p-[2px] opacity-0 transition-opacity duration-500 group-hover:opacity-100'></span>
-
-                    <span class='relative z-10 block px-6 py-3 rounded-xl bg-[#02101c]'>
-                      <div class='relative z-10 flex items-center space-x-2'>
-                        <span class='transition-all duration-500 group-hover:translate-x-1'>
-                          Get Started
-                        </span>
-                        <VscDebugStart className='w-6 h-6 transition-transform duration-500 group-hover:translate-x-1' />
-                      </div>
-                    </span>
-                  </button>
-                </div>
+                <MainButton
+                  text='Get Started'
+                  icon={VscDebugStart}
+                  onClick={() => console.log('Get Started Clicked')}
+                />
 
                 <div className='relative group'>
                   <button class='relative inline-block p-px font-semibold leading-6 text-[#02101c] bg-[#30e4ba]  shadow-lg cursor-pointer rounded-xl shadow-zinc-900 transition-transform duration-300 ease-in-out hover:scale-105 active:scale-95'>
@@ -52,8 +44,8 @@ const Hero = () => {
             </div>
           </div>
           <img
-            src='/images/placeholders/minima/placeholder-wide-1.jpg'
-            alt='placeholder'
+            src={heroImg}
+            alt='Hero Image'
             class='mx-auto mt-24 max-h-[700px] w-full max-w-7xl rounded-t-lg object-cover shadow-lg'
           />
         </div>
