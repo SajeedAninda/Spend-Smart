15283a202663148ace98544ca535be13a50cc52a
SajeedAninda <sajaninda@gmail.com>
2025-02-01 18:32:09 +0600

    Created testimonial section header

--- src/Components/Navbar/Pages/Homepage/Homepage.jsx
+++ src/Components/Navbar/Pages/Homepage/Homepage.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Hero from './Hero/Hero';
 import Benefits from './Benefits/Benefits';
+import Testimonial from './Testimonial/Testimonial';
 
 const Homepage = () => {
     return (
         <>
            <Hero></Hero> 
            <Benefits></Benefits>
+           <Testimonial></Testimonial>
         </>
     );
 };
---
+++ src/Components/Navbar/Pages/Homepage/Testimonial/Testimonial.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Testimonial = () => {
+  return (
+    <section className='pb-32'>
+      <div className='w-[1000px] mx-auto flex max-w-screen-md flex-col items-center gap-6'>
+        <h2 className='mb-4 text-center text-3xl font-semibold lg:text-5xl'>
+          Discover why People Loves Spend Smart
+        </h2>
+        <p className='text-center text-zinc-600 lg:text-2xl'>
+          From saving for a rainy day to planning for big life milestones, Spend
+          Smart has helped over 100,000 users achieve financial freedom.
+        </p>
+      </div>
+    </section>
+  )
+}
+
+export default Testimonial
