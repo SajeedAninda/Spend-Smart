2f72be738d726590c2c1a533ab83450f1b72ef6b
SajeedAninda <sajaninda@gmail.com>
2025-02-03 17:12:19 +0600

    Login with google and post google login data

--- src/Components/Authentication/SocialLogin.jsx
+++ src/Components/Authentication/SocialLogin.jsx
@@ -10,11 +10,35 @@ const SocialLogin = () => {
   let axiosInstance = useAxiosInstance()
   let navigate = useNavigate()
 
- 
+  let handleGoogleLogin = () => {
+    googleLogin()
+      .then(result => {
+        const user = result.user
+        let userDetails = {
+          name: user?.displayName,
+          email: user?.email,
+          imageUrl: user?.photoURL
+        }
+
+        axiosInstance.post('/userGoogleRegister', userDetails).then(res => {
+          console.log(res.data)
+          if (res.data.insertedId) {
+            console.log(res.data)
+          }
+        })
+        toast.success('Logged In Successfully!', {
+          duration: 3000
+        })
+        navigate('/')
+      })
+      .catch(error => {
+        console.log(error)
+      })
+  }
 
   return (
     <div className='relative group mt-4 w-full'>
-      <button className='relative w-full flex justify-center p-px font-semibold leading-6 text-[#02101c] bg-[#30e4ba] shadow-md cursor-pointer rounded-xl shadow-zinc-900 transition-transform duration-300 ease-in-out hover:scale-105 active:scale-95'>
+      <button onClick={handleGoogleLogin} className='relative w-full flex justify-center p-px font-semibold leading-6 text-[#02101c] bg-[#30e4ba] shadow-md cursor-pointer rounded-xl shadow-zinc-900 transition-transform duration-300 ease-in-out hover:scale-105 active:scale-95'>
         <span className='relative z-10 block px-6 py-3 rounded-xl bg-[#30e4ba]'>
           <div className='relative z-10 flex items-center space-x-2'>
             <FaGoogle className='w-6 h-6 transition-transform duration-500 group-hover:translate-x-1' />
