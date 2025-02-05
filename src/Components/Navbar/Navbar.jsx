d2fd23101c445dc20d6a55709c40ee7f0518b972
SajeedAninda <sajaninda@gmail.com>
2025-02-05 14:24:50 +0600

    Created Links in Navbar

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -34,27 +34,46 @@ const Navbar = () => {
 
   return (
     <div className='w-full h-[14vh] bg-gradient-to-r from-[#30e4ba] via-white to-[#30e4ba] flex items-center'>
-      <div className='w-[1000px] mx-auto flex justify-between items-center'>
-        {/* Brand Name + Tagline */}
-        <div className='w-[33%] flex flex-col items-center text-center'>
-          <h1 className='text-[#02101c] font-black text-[16px] italic'>
-            SpendSmart
-          </h1>
-          <TypeAnimation
-            sequence={[
-              'Make Every Penny Count',
-              1000,
-              'Smarter Choices, Bigger Savings',
-              1000,
-              'Spend Wisely, Live Freely',
-              1000,
-              'Budget Better, Live Smarter',
-              1000
-            ]}
-            speed={50}
-            className='text-[#02101c] font-black italic text-[16px]'
-            repeat={Infinity}
-          />
+      <div className='w-[1150px] mx-auto flex justify-between items-center'>
+        {/* Brand Name + Tagline or Links*/}
+        <div className='w-[33%]'>
+          {userData ? (
+            <div className='flex justify-between items-center '>
+              <li className='text-[#02101c] font-bold hover:opacity-50 transition-all duration-150 hover:underline cursor-pointer text-[16px] list-none'>
+                Transactions
+              </li>
+              <li className='text-[#02101c] font-bold hover:opacity-50 transition-all duration-150 hover:underline cursor-pointer text-[16px] list-none'>
+                Budgets
+              </li>
+              <li className='text-[#02101c] font-bold hover:opacity-50 transition-all duration-150 hover:underline cursor-pointer text-[16px] list-none'>
+                Piggy Bank
+              </li>
+              <li className='text-[#02101c] font-bold hover:opacity-50 transition-all duration-150 hover:underline cursor-pointer text-[16px] list-none'>
+                Bills
+              </li>
+            </div>
+          ) : (
+            <div className='flex flex-col items-center text-center'>
+              <h1 className='text-[#02101c] font-black text-[16px] italic'>
+                SpendSmart
+              </h1>
+              <TypeAnimation
+                sequence={[
+                  'Make Every Penny Count',
+                  1000,
+                  'Smarter Choices, Bigger Savings',
+                  1000,
+                  'Spend Wisely, Live Freely',
+                  1000,
+                  'Budget Better, Live Smarter',
+                  1000
+                ]}
+                speed={50}
+                className='text-[#02101c] font-black italic text-[16px]'
+                repeat={Infinity}
+              />
+            </div>
+          )}
         </div>
 
         {/* Logo */}
@@ -68,7 +87,11 @@ const Navbar = () => {
         <div className='w-[33%] flex items-center justify-end space-x-4'>
           {userData && (
             <div>
-              <img className='w-[50px] h-[50px] rounded-full border-2 object-cover border-[#02101c]' src={userData?.imageUrl} alt='' />
+              <img
+                className='w-[50px] h-[50px] rounded-full border-2 object-cover border-[#02101c]'
+                src={userData?.imageUrl}
+                alt=''
+              />
             </div>
           )}
           {userData ? (
