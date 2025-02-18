bf3746266065678a19c2220f8eef1aae39713ef5
SajeedAninda <sajaninda@gmail.com>
2025-02-18 17:21:47 +0600

    Created functionality to add money

--- src/Components/Pages/PiggyBank/AddMoneyModal.jsx
+++ src/Components/Pages/PiggyBank/AddMoneyModal.jsx
@@ -1,26 +1,65 @@
 import React, { useEffect, useState } from 'react'
 import { IoCheckmarkDoneCircle, IoClose } from 'react-icons/io5'
+import useAxiosInstance from '../../Hooks/useAxiosInstance'
+import toast from 'react-hot-toast'
 
 const AddMoneyModal = ({ piggyBank, onClose, refetch }) => {
   const targetSpend = parseFloat(piggyBank?.targetSpend) || 0
   const availableBalance = parseFloat(piggyBank?.availableBalance) || 0
   const maxAddableAmount = targetSpend - availableBalance
+  const axiosInstance = useAxiosInstance()
 
   const [addedAmount, setAddedAmount] = useState(0)
 
-  const handleAmountChange = (e) => {
+  const handleAmountChange = e => {
     let value = parseFloat(e.target.value) || 0
-    if (value > maxAddableAmount) value = maxAddableAmount 
+    if (value > maxAddableAmount) value = maxAddableAmount
     setAddedAmount(value)
   }
 
   const newTotal = availableBalance + addedAmount
   const progress = (newTotal / targetSpend) * 100 || 0
 
+  const handleAddAmount = async e => {
+    e.preventDefault()
+    if (!addedAmount) {
+      return toast.error('Please Type Amount!')
+    }
+    let loadingToast = toast.loading('Adding Money...')
+
+    try {
+      const { data } = await axiosInstance.patch(
+        `/addMoney/${piggyBank?._id}`,
+        {
+          addedAmount
+        }
+      )
+      if (data.modifiedCount > 0) {
+        toast.success('Money Added Succesfully!')
+        refetch()
+        onClose()
+      } else {
+        toast.error('No changes made or update failed.')
+      }
+    } catch (error) {
+      toast.error('Error adding Money!')
+    }
+    toast.dismiss(loadingToast)
+  }
+
   return (
-    <div className='fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-50' onClick={onClose}>
-      <div className='bg-white w-[40%] p-6 rounded-lg shadow-lg relative' onClick={e => e.stopPropagation()}>
-        <button className='absolute top-3 right-3 text-gray-600 hover:text-black' onClick={onClose}>
+    <div
+      className='fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-50'
+      onClick={onClose}
+    >
+      <div
+        className='bg-white w-[40%] p-6 rounded-lg shadow-lg relative'
+        onClick={e => e.stopPropagation()}
+      >
+        <button
+          className='absolute top-3 right-3 text-gray-600 hover:text-black'
+          onClick={onClose}
+        >
           <IoClose size={24} />
         </button>
 
@@ -31,21 +70,33 @@ const AddMoneyModal = ({ piggyBank, onClose, refetch }) => {
           </h1>
 
           <h3 className='text-[14px] mt-2 text-[#5b5d5f] font-semibold'>
-          Add money to your Piggy Bank to reach your target. As soon as you hit
-            confirm, the balance will be added to your Piggy Bank. The summation
-            of money cannot be more than the targeted amount.
+            Add money to your Piggy Bank to reach your target. As soon as you
+            hit confirm, the balance will be added to your Piggy Bank. The
+            summation of money cannot be more than the targeted amount.
           </h3>
 
           <div>
             <div className='flex justify-between items-center'>
               <p className='text-[#02101c] text-sm '>Total Saved</p>
-              <h2 style={{ color: piggyBank?.colorTheme }} className='text-3xl font-bold '>
+              <h2
+                style={{ color: piggyBank?.colorTheme }}
+                className='text-3xl font-bold '
+              >
                 ${newTotal.toFixed(2)}
               </h2>
             </div>
 
-            <div className='relative h-2 w-full my-6 rounded-lg overflow-hidden' style={{ backgroundColor: `${piggyBank?.colorTheme}70` }}>
-              <div className='h-full rounded-lg' style={{ width: `${progress}%`, backgroundColor: piggyBank?.colorTheme }} />
+            <div
+              className='relative h-2 w-full my-6 rounded-lg overflow-hidden'
+              style={{ backgroundColor: `${piggyBank?.colorTheme}70` }}
+            >
+              <div
+                className='h-full rounded-lg'
+                style={{
+                  width: `${progress}%`,
+                  backgroundColor: piggyBank?.colorTheme
+                }}
+              />
             </div>
 
             <div className='flex justify-between text-[14px] font-bold text-[#02101c] my-2'>
@@ -54,10 +105,12 @@ const AddMoneyModal = ({ piggyBank, onClose, refetch }) => {
             </div>
           </div>
 
-          <form className='mt-4'>
+          <form onSubmit={handleAddAmount} className='mt-4'>
             {/* Amount Input */}
             <div>
-              <label className='text-[14px]' htmlFor='max_spend'>Amount to Add</label>
+              <label className='text-[14px]' htmlFor='max_spend'>
+                Amount to Add
+              </label>
               <input
                 className='w-full py-3 px-4 rounded-lg border mt-2 placeholder:text-[14px] placeholder:text-gray-500 border-[#02101c]'
                 placeholder={`Max: $${maxAddableAmount.toFixed(2)}`}
