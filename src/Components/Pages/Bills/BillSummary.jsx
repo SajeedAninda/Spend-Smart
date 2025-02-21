12502ed41b142b5903b7b164a997d6eeecd1b3a7
SajeedAninda <sajaninda@gmail.com>
2025-02-21 18:17:25 +0600

    Completed Bills summary

--- src/Components/Pages/Bills/BillSummary.jsx
+++ src/Components/Pages/Bills/BillSummary.jsx
@@ -1,27 +1,39 @@
-import React from 'react'
+import React from 'react';
 
-const BillSummary = () => {
+const BillSummary = ({ allBills }) => {
+  const today = new Date().getDate(); 
+
+
+  const paidBills = allBills?.filter(bill => bill.billStatus === 'paid');
+  const paidTotal = paidBills?.reduce((sum, bill) => sum + parseFloat(bill.billingAmount), 0);
+
+  const dueBills = allBills?.filter(bill => bill.billStatus === 'unpaid' && parseInt(bill.billDueDay) < today);
+  const dueTotal = dueBills?.reduce((sum, bill) => sum + parseFloat(bill.billingAmount), 0);
+
+  const upcomingBills = allBills?.filter(bill => bill.billStatus === 'unpaid' && parseInt(bill.billDueDay) >= today);
+  const upcomingTotal = upcomingBills?.reduce((sum, bill) => sum + parseFloat(bill.billingAmount), 0);
+
   return (
-    <div className='bg-[#cbfdf2]  h-[200px] rounded-lg py-6 px-10'>
+    <div className='bg-[#cbfdf2] h-[200px] rounded-lg py-6 px-10'>
       <p className='text-[18px] font-bold text-[#02101c]'>Bills Summary</p>
       <div>
         <div className='flex justify-between mt-5 text-green-700'>
-          <p className='text-[16px] font-bold '>Paid Bills</p>
-          <p className='text-[16px] font-bold '>5 (Total: $ 450)</p>
+          <p className='text-[16px] font-bold'>Paid Bills</p>
+          <p className='text-[16px] font-bold'>{paidBills?.length} (Total: $ {paidTotal?.toFixed(2)})</p>
         </div>
 
         <div className='flex justify-between mt-5 text-red-700'>
-          <p className='text-[16px] font-bold '>Due Bills</p>
-          <p className='text-[16px] font-bold '>2 (Total: $ 410)</p>
+          <p className='text-[16px] font-bold'>Due Bills</p>
+          <p className='text-[16px] font-bold'>{dueBills?.length} (Total: $ {dueTotal?.toFixed(2)})</p>
         </div>
 
         <div className='flex justify-between mt-5 text-yellow-700'>
-          <p className='text-[16px] font-bold '>Upcoming Bills</p>
-          <p className='text-[16px] font-bold '>3 (Total: $ 190)</p>
+          <p className='text-[16px] font-bold'>Upcoming Bills</p>
+          <p className='text-[16px] font-bold'>{upcomingBills?.length} (Total: $ {upcomingTotal?.toFixed(2)})</p>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default BillSummary
+export default BillSummary;
