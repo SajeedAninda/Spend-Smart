93a7e966e84f05da8815937b87e9e58945a113ab
SajeedAninda <sajaninda@gmail.com>
2025-02-24 18:02:48 +0600

    Made rest of the modals responsive

--- src/Components/Pages/PiggyBank/PiggyUpdateModal.jsx
+++ src/Components/Pages/PiggyBank/PiggyUpdateModal.jsx
@@ -62,7 +62,7 @@ const PiggyUpdateModal = ({ piggyBank, onClose, refetch }) => {
       onClick={onClose}
     >
       <div
-        className='bg-white w-[40%] p-6 rounded-lg shadow-lg relative'
+        className='bg-white w-[90%] lg:w-[40%] p-6 rounded-lg shadow-lg relative'
         onClick={e => e.stopPropagation()}
       >
         <button
