c3a1427c583ed4944b6bdded01e6b8ba51c7320e
SajeedAninda <sajaninda@gmail.com>
2025-02-28 16:46:51 +0600

    Used two toggle buttons in navbar

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -103,6 +103,11 @@ const Navbar = () => {
                 onClick={toggleSidebar}
               />
               <div className='hidden md:flex items-center space-x-4'>
+                <label class='relative inline-flex items-center cursor-pointer'>
+                  <input class='sr-only peer' value='' type='checkbox' />
+                  <div class="darkModeToggler w-20 h-8 rounded-full ring-0 peer duration-500 outline-none bg-[#24aa8b] overflow-hidden before:flex before:items-center before:justify-center after:flex after:items-center after:justify-center before:content-['☀️'] before:absolute before:h-7 before:w-10 before:top-1/2 before:bg-white before:rounded-full before:left-1 before:-translate-y-1/2 before:transition-all before:duration-700 peer-checked:before:opacity-0 peer-checked:before:rotate-90 peer-checked:before:-translate-y-full shadow-lg shadow-gray-400 peer-checked:shadow-lg peer-checked:shadow-gray-700 peer-checked:bg-[#383838] after:content-['🌑'] after:absolute after:bg-[#02101c] after:rounded-full after:top-[4px] after:right-1 after:translate-y-full after:w-10 after:h-7 after:opacity-0 after:transition-all after:duration-700 peer-checked:after:opacity-100 peer-checked:after:rotate-180 peer-checked:after:-translate-y-0.5"></div>
+                </label>
+
                 <img
                   className='w-[50px] h-[50px] rounded-full border-2 object-cover border-[#02101c]'
                   src={userData?.imageUrl}
@@ -116,13 +121,19 @@ const Navbar = () => {
               </div>
             </>
           ) : (
-            <Link to={'/login'}>
-              <MainButton
-                text='Login'
-                icon={IoLogIn}
-                onClick={() => console.log('Login Clicked')}
-              />
-            </Link>
+            <div className='flex items-center space-x-4'>
+              <label class='relative inline-flex items-center cursor-pointer'>
+                  <input class='sr-only peer' value='' type='checkbox' />
+                  <div class="darkModeToggler w-20 h-8 rounded-full ring-0 peer duration-500 outline-none bg-[#24aa8b] overflow-hidden before:flex before:items-center before:justify-center after:flex after:items-center after:justify-center before:content-['☀️'] before:absolute before:h-7 before:w-10 before:top-1/2 before:bg-white before:rounded-full before:left-1 before:-translate-y-1/2 before:transition-all before:duration-700 peer-checked:before:opacity-0 peer-checked:before:rotate-90 peer-checked:before:-translate-y-full shadow-lg shadow-gray-400 peer-checked:shadow-lg peer-checked:shadow-gray-700 peer-checked:bg-[#383838] after:content-['🌑'] after:absolute after:bg-[#1d1d1d] after:rounded-full after:top-[4px] after:right-1 after:translate-y-full after:w-10 after:h-7 after:opacity-0 after:transition-all after:duration-700 peer-checked:after:opacity-100 peer-checked:after:rotate-180 peer-checked:after:translate-y-0"></div>
+                </label>
+              <Link to={'/login'}>
+                <MainButton
+                  text='Login'
+                  icon={IoLogIn}
+                  onClick={() => console.log('Login Clicked')}
+                />
+              </Link>
+            </div>
           )}
         </div>
       </div>
