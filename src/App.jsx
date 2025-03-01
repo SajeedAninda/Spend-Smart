ac73ec57b141ec6caa25b31fdee8be0c50292cea
SajeedAninda <sajaninda@gmail.com>
2025-03-01 16:24:35 +0600

    Implemented react scroll to top

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
-import { Outlet } from "react-router-dom"
-import Navbar from "./Components/Navbar/Navbar"
-import Footer from "./Components/Footer/Footer"
+import { Outlet } from 'react-router-dom'
+import Navbar from './Components/Navbar/Navbar'
+import Footer from './Components/Footer/Footer'
+import ScrollToTop from './Components/ScrollTopTop/ScrollToTop'
 
-function App() {
-
+function App () {
   return (
     <>
+      <ScrollToTop></ScrollToTop>
       <Navbar></Navbar>
       <Outlet></Outlet>
       <Footer></Footer>
